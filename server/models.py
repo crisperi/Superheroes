2c1dcbc0889af8045fdd85fbe3b42699ea45513d
crisperi <phoenixedian@gmail.com>
2025-06-25 14:46:28 +0300

    added validations

--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Model
+from sqlalchemy.orm import validates
 
 
 metadata = MetaData(
@@ -27,12 +28,27 @@ class Power(db.model):
     name =db.Column(db.String)
     description = db.Column(db.String)
     
-    
+    @validates("description")
+    def validate_description(self, key, value):
+        if len(value) < 20:
+            raise ValueError("Description must be at least 20 characters long.")
+        return value
+
 class HeroPower(db.model):
     __tablename__ = "hero_powers"
     
     strength=db.Column(db.String)
     hero_id =db.Column(db.Integer,db.ForeignKey("heroes.id"))
     power_id =db.Column(db.Integer,db.ForeignKey("powers.id"))
+    
+    @validates("strength")
+    def validate_strength(self,key,value ):
+        allowed =["Strong","Weak","Average"]
+        
+        if value not in allowed :
+            raise ValueError(f"Strength must be one of {allowed}")
+        return value
+    
+    
     
     
