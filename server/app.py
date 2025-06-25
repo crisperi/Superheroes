ae72f263794d737517cc2fb783114a8842d93157
crisperi <phoenixedian@gmail.com>
2025-06-25 14:56:52 +0300

    added resources folders

--- server/app.py
+++ server/app.py
@@ -8,6 +8,7 @@ app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
 app.json.compact =False
 migrate = Migrate(app,db)
+
 db.init_app(app)
 
 if __name__=="__main__":
